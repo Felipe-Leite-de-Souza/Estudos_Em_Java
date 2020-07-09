ff51997717263b954a8cf87c12d0ce7c323155ee
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-09 07:37:21 -0300

    Exercício de cálculo de média de valores

---
+++ src/exemplosBasicos/ExemploCalculoDeMedia.java
@@ -0,0 +1,22 @@
+package exemplosBasicos;
+
+public class ExemploCalculoDeMedia {
+
+	public static void main(String[] args) {
+		int preco1 = 5;
+		int preco2 = 8;
+		int preco3 = 12;
+		
+		int precoTotal = preco1 + preco2 + preco3;
+		
+		int precoMedio = precoTotal / 3;
+		
+		System.out.println("\"Sistema de Cálculo de Média\"");
+		System.out.println("Preço 1: " + preco1);
+		System.out.println("Preço 2: " + preco2);
+		System.out.println("Preço 3: " + preco3);
+		System.out.println("Preço médio: " + precoMedio);
+
+	}
+
+}
