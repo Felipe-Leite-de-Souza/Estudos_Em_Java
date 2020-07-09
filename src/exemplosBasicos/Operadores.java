f7cac683fc4dd8df2c306e9d7937f9fa03ff91be
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-09 02:02:13 -0300

    criação de exemplo dos tipos de operadores

---
+++ src/exemplosBasicos/Operadores.java
@@ -0,0 +1,25 @@
+package exemplosBasicos;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		int total = 10;
+		total += 3;
+		
+		System.out.println(total);
+		
+		total -= 2;
+		System.out.println(total);
+		
+		total *= 3;
+		System.out.println(total);
+		
+		total /= 3;
+		System.out.println(total);
+		
+		total %= 5;
+		System.out.println(total);
+	
+	}
+
+}
