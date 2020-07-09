29802c082c8f00816531f69d9b8e9b504aecbd45
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-09 02:40:02 -0300

    criação de exemplo de operadores de comparacao e igualdade

---
+++ src/exemplosBasicos/OperadoresDeComparacaoEIgualdade.java
@@ -0,0 +1,14 @@
+package exemplosBasicos;
+
+public class OperadoresDeComparacaoEIgualdade {
+
+	public static void main(String[] args) {
+		boolean bloqueado = false;
+
+		bloqueado = ! bloqueado;
+
+		System.out.println(bloqueado);
+
+	}
+
+}
