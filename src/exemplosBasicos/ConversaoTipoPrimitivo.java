19dd0c73eac7458c2f571e77ea18cf41daa11158
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-09 01:54:38 -0300

    criação do exemplos dos tipos primitivos

---
+++ src/exemplosBasicos/ConversaoTipoPrimitivo.java
@@ -0,0 +1,14 @@
+package exemplosBasicos;
+
+public class ConversaoTipoPrimitivo {
+
+	public static void main(String[] args) {
+		double largura = 100.37;
+
+		int tamanho = (int) largura;
+
+		System.out.println(tamanho);
+
+	}
+
+}
