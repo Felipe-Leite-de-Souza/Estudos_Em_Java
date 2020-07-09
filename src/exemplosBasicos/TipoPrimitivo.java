e4735dacb549fe934a19ea844f985bf312a9ed49
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-09 02:38:21 -0300

    criação de exemplo de tipo primitivo - 1

---
+++ src/exemplosBasicos/TipoPrimitivo.java
@@ -0,0 +1,35 @@
+package exemplosBasicos;
+
+public class TipoPrimitivo {
+
+	public static void main(String[] args) {
+		double precoProduto = 9.43;
+
+		System.out.println("Preço do produto: " + precoProduto + "\n");
+
+		boolean alunoMatriculado = true;
+		boolean clienteBloqueado = false;
+
+		System.out.println("Aluno matriculado: " + alunoMatriculado);
+		System.out.println("Cliente bloqueado: " + clienteBloqueado+ "\n");
+
+		char turmaAluno = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+
+		System.out.println("Turma: " + turmaAluno);
+		System.out.println("Tipo Cliente: " + tipoCliente);
+		System.out.println("Simbolo: " + simbolo + "\n");
+
+		long populacaoCidade = 650000;
+		System.out.println("População da cidade: " + populacaoCidade + "\n");
+
+		long populacaoMundial = 7000000000L;
+		System.out.println("Populacao Mundial: " + populacaoMundial + "\n");
+
+		float saldoConta = 1030.43F;
+		System.out.println("Saldo da conta: " + saldoConta);
+
+	}
+
+}
