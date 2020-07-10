1f10d8e8cd4fba4ac5dcb965f9b037385885071c
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-10 03:08:23 -0300

    Exemplo de array

---
+++ src/arrayTeste/TesteArrays.java
@@ -0,0 +1,21 @@
+package arrayTeste;
+
+public class TesteArrays {
+
+	public static void main(String[] args) {
+		int [] notas = new int[4];
+		notas[0] = 10;
+		notas[1] = 5;
+		notas[2] = 8;
+		notas[3] = 9;
+		
+		System.out.println("Notas do primeiro aluno: ");
+		System.out.println("Tamanho do array: " + notas.length);
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Posição [" + i + "] = " + notas[i]);
+		}
+
+	}
+
+}
