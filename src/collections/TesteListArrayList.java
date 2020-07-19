225e103de7c62bdd4dc1958d9679dc7315dad154
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-19 03:38:34 -0300

    Collections com ArrayList<E> utilizando For Iterator

---
+++ src/collections/TesteListArrayList.java
@@ -0,0 +1,32 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteListArrayList {
+
+	public static void main(String[] args) {
+		Aluno aluno1 = new Aluno("Fernanda");
+		Aluno aluno2 = new Aluno("Isis");
+		Aluno aluno3 = new Aluno("Thiago");
+		Aluno aluno4 = new Aluno("Alberto");
+		
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		alunos.add(aluno1);
+		alunos.add(aluno2);
+		alunos.add(aluno3);
+		alunos.add(aluno4);
+		
+	}
+	
+	//Declarado com STATIC pois será chamado dentro do método main
+	public static void imprimirAlunos(List<Aluno> alunos) {
+		//For Iterator
+		for (Aluno aluno : alunos) {
+			System.out.println("Nome do aluno: " + aluno.getNome());
+		}
+	}
+
+}
