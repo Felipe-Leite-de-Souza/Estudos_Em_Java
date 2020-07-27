4c865da1f51ad717b322549b3b622a183f1da624
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-27 15:58:33 -0300

    Estudando um pouco mais sobre javadoc

---
+++ src/exemplojavadoc/Viagem.java
@@ -0,0 +1,51 @@
+package exemplojavadoc;
+
+import java.util.Date;
+
+/**
+ * 
+ * Representa a viagem de um funcionários.
+ * 
+ * @author Felipe Leite de Souza
+ *
+ */
+
+
+public class Viagem {
+	private Date dataInicio;
+	private Date dataFim;
+	private Funcionario funcionario;
+	
+	// Exemplificando
+	
+	public Viagem(Date dataInicio, Date dataFim, Funcionario funcionario) {
+		this.dataInicio = dataInicio;
+		this.dataFim = dataFim;
+		this.funcionario = funcionario;
+	}
+
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+}
