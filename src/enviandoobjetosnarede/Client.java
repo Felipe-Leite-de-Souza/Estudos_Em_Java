715663aa86d260982b6bdc6cb0f33c2db4fbbc30
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-31 18:15:30 -0300

    Enviando objetos na rede com Socket

---
+++ src/enviandoobjetosnarede/Client.java
@@ -0,0 +1,33 @@
+package enviandoobjetosnarede;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+	
+	public static void main(String[] args) throws Exception {
+		Socket socket = new Socket("192.168.0.119", 3333);
+		
+		OutputStream saida = socket.getOutputStream();
+		ObjectOutput objectOutput = new ObjectOutputStream(saida);
+		
+		Pedido pedido = new Pedido();
+		pedido.setCodigo(1L);
+		pedido.setDescricao("Barra de Chocolate");
+		pedido.setQuantidade(3);
+		
+		objectOutput.writeObject(pedido);
+		
+		InputStream entrada = socket.getInputStream();
+		DataInput dataInput = new DataInputStream(entrada);
+		System.out.println("Recebeu do servidor: " + dataInput.readUTF());
+		
+		socket.close();
+		
+	}
+}
