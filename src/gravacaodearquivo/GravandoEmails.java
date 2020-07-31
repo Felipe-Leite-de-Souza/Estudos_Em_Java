5b03af3f1d7b35e40fbdc19cc4a0637c5ac7cf72
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-07-30 21:15:09 -0300

    Gravando Arquivos

---
+++ src/gravacaodearquivo/GravandoEmails.java
@@ -0,0 +1,32 @@
+package gravacaodearquivo;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class GravandoEmails {
+
+	public static void main(String[] args) {
+		String[] emails = {"java2@java.com", "python2@python.com", "ruby2@ruby.com"};
+		
+		//Uma das formas de fazer
+		/*try (BufferedWriter writer = new BufferedWriter(new FileWriter("emails.txt", true))) {
+		
+			writer.write(email);
+			writer.newLine();
+		*/
+		
+		//Outra forma de fazer
+		try (PrintStream writer = new PrintStream("emails2.txt")) {	
+			for (String email : emails) {
+
+				writer.println(email);
+			}
+		} catch (IOException e) {
+			System.err.println("Não conseguiu gravar o arquivo." + e.getMessage());
+		} 
+
+	}
+
+}
