4a3e901734716a1ca1844eefab530ea095a7b2f4
Felipe Leite de Souza <felipe_leitesouza@hotmail.com>
2020-08-01 17:38:10 -0300

    Adicionando comentário sobre o uso do recurso do Java 7 Diamond.

--- src/lambda/FaturaBuscarBanco.java
+++ src/lambda/FaturaBuscarBanco.java
@@ -8,6 +8,16 @@ public class FaturaBuscarBanco {
 	public List<Fatura> buscarFaturasVencidas() {
 		// Obviamente você buscaria isso no banco de dados...
 		
+		/*
+		 * Diamond do Java 7 com ele não necessitamos afirmar o 
+		 * tipo do array que queremos
+		 */
+		
+		//Sem Diamond
+		
+		//List<Fatura> faturas = new ArrayList<Fatura>();
+		
+		//Com Diamond
 		List<Fatura> faturas = new ArrayList<>();
 		faturas.add(new Fatura("devedor01@hotmail.com", 384.95));
 		faturas.add(new Fatura("devedor02@gmail.com", 149.90));
